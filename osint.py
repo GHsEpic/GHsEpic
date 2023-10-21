5bd2a162be06e566cd32c1aa5f24aaaff8b42368
GHsEpic <148632096+GHsEpic@users.noreply.github.com>
2023-10-21 15:20:49 +0200

    Update osint.py

--- osint.py
+++ osint.py
@@ -2,19 +2,19 @@ import googlesearch as gs
 import requests as req
 
 def google(prompt, n):
-    s = gs.search(prompt, stop=n)
-    results = [_ for _ in s]
-    return results
+	s = gs.search(prompt, stop=n)
+	results = [_ for _ in s]
+	return results
         
 def eval_link(link, keywords):
-    data = {}
-    if not (link.startswith("http://") or link.startswith("https://")):
-        link = "http://"+link
+	data = {}
+	if not (link.startswith("http://") or link.startswith("https://")):
+		link = "http://"+link
 
-    response = req.get(link)
-    data["Content-Type"] = response.headers["Content-Type"]
-    content = response.content.decode(response.encoding or "UTF-8", errors="ignore").split(">")
-    content = [_+">" for _ in content]
+	response = req.get(link)
+	data["Content-Type"] = response.headers["Content-Type"]
+	content = response.content.decode(response.encoding or "UTF-8", errors="ignore").split(">")
+	content = [_+">" for _ in content]
 
 	data["found_keywords"] = {}
 	for _ in content:

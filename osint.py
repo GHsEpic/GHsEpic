b911fda505bc99f721a7cba46d8b145e77986400
GHsEpic <148632096+GHsEpic@users.noreply.github.com>
2023-10-21 15:39:58 +0200

    Update osint.py

--- osint.py
+++ osint.py
@@ -13,13 +13,25 @@ def eval_link(link, keywords):
 
 	response = req.get(link)
 	data["Content-Type"] = response.headers["Content-Type"]
-	content = response.content.decode(response.encoding or "UTF-8", errors="ignore").split(">")
-	content = [_+">" for _ in content]
+	content = response.content.decode(response.encoding or "UTF-8", errors="ignore").replace(" ", "").split("\n")
 
 	data["found_keywords"] = {}
+	important = []
 	for _ in content:
 		for kw in keywords:
+			data["found_keywords"][kw] = []
 			if kw.lower() in _.lower():
-				data["found_keywords"][kw] = _
+				data["found_keywords"][kw].append(_)
+				important.append(_)
+	
+	data["found_links"] = []
+	for _ in important:
+		if "href=" in _:
+			_ = _.split("'").split('"')
+			for i, . in enumerate(_):
+				if "href=" in .:
+					link_index = i+1
+					break
+		data["found_links"].append(_[link_index])
 
 	return data
